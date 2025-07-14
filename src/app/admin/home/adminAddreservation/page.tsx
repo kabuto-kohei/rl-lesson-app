c9eef74eb46d63dba1a3b36260ae46ebc08b351d
加部東航平 <kabuclimbing@gmail.com>
2025-07-14 21:36:46 +0900

    頼む1

--- src/app/admin/home/adminAddreservation/page.tsx
+++ src/app/admin/home/adminAddreservation/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+export const dynamic = 'force-dynamic'; // ← これが重要！
+
 import { useSearchParams, useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import styles from './page.module.css';
